7c948dd9d4947e5640632c218d1d4f4182b9c13c
Renan Machado <renan.mav@hotmail.com>
2018-12-15 02:50:49 -0300

    Reactotron config

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+import Map from './pages/Map/index';
+
+import './config/Reactotron';
+import store from './redux/store';
+
+const App = () => (
+  <Provider store={store}>
+    <Map />
+  </Provider>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Map from './pages/Map';
+import App from './App';
 import './styles/index.css';
 
-ReactDOM.render(<Map />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
